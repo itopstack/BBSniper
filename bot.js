0359be8f7f180a13cb70dfaf8a1811e4f61b98eb
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-07-31 23:01:10 +0700

    Approve, check profit, and sell token

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const ethers = require("ethers");
 const dotenv = require("dotenv");
+const fetch = require("node-fetch");
 
 dotenv.config();
 
@@ -16,7 +17,9 @@ const provider = new ethers.providers.WebSocketProvider(node);
 const wallet = new ethers.Wallet(privateKey);
 const account = wallet.connect(provider);
 const recipient = account.address;
-const minBnbForPair = 1;
+const minBnbForPair = 10;
+const myGasPrice = ethers.utils.parseUnits("5", "gwei");
+const profitXAmount = 2;
 
 const factory = new ethers.Contract(
   addresses.factory,
@@ -31,6 +34,7 @@ const router = new ethers.Contract(
   [
     "function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts)",
     "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)",
+    "function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
   ],
   account
 );
@@ -50,7 +54,22 @@ const erc = new ethers.Contract(
   account
 );
 
+const tokenAbi = [
+  "function approve(address spender, uint amount) public returns(bool)",
+  "function balanceOf(address account) external view returns (uint256)",
+  "function decimals() external view returns (uint8)",
+  "event Transfer(address indexed from, address indexed to, uint amount)",
+  "function transfer(address to, uint amount) returns (bool)",
+];
+
+let isSniping = false;
+
 factory.on("PairCreated", async (token0, token1, addressPair) => {
+  if (isSniping) {
+    console.log("Already snipe some token. Wait until that task finished");
+    return;
+  }
+
   console.log(`
     ~~~~~~~~~~~~~~~~~~
     New pair detected
@@ -85,6 +104,16 @@ factory.on("PairCreated", async (token0, token1, addressPair) => {
     return;
   }
 
+  console.log("Checking honey pot...");
+  const response = await fetch(
+    `https://aywt3wreda.execute-api.eu-west-1.amazonaws.com/default/IsHoneypot?chain=bsc2&token=${sellToken}`
+  );
+  const json = await response.json();
+  if (json.IsHoneypot) {
+    console.log("Token is honey pot. Skip it.");
+    return;
+  }
+
   const amountIn = ethers.utils.parseUnits("0.001", "ether"); //ether is the measurement, not the coin
   const amounts = await router.getAmountsOut(amountIn, [buyToken, sellToken]);
 
@@ -106,6 +135,60 @@ factory.on("PairCreated", async (token0, token1, addressPair) => {
   );
 
   const receipt = await tx.wait();
-  console.log("Transaction receipt");
+  console.log("Buy transaction receipt");
   console.log(receipt);
+
+  isSniping = true;
+
+  const contract = new ethers.Contract(sellToken, tokenAbi, account);
+
+  const valueToApprove = ethers.utils.parseUnits("0", "ether");
+  const approvedTx = await contract.approve(addresses.router, valueToApprove, {
+    gasPrice: myGasPrice,
+    gasLimit: 210000,
+  });
+
+  console.log("Approving sell token...");
+  const approvedReceipt = await approvedTx.wait();
+  console.log("Approved transaction receipt");
+  console.log(approvedReceipt);
+
+  contract.on("Transfer", async (from, to, value, event) => {
+    console.log("Check profit...");
+
+    const bal = await contract.balanceOf(recipient);
+    const amount = await router.getAmountsOut(bal, [sellToken, buyToken]);
+    const profitDesired = amountIn.mul(profitXAmount);
+    const currentValue = amount[1];
+
+    console.log(
+      "Current Value:",
+      ethers.utils.formatUnits(currentValue),
+      "Profit Wanted:",
+      ethers.utils.formatUnits(profitDesired)
+    );
+
+    if (currentValue.gte(profitDesired)) {
+      console.log("Selling token to take profit...");
+
+      const amountsOutMin = currentValue.sub(currentValue.div(10));
+      const tx = await router.swapExactTokensForETH(
+        amount[0],
+        amountsOutMin,
+        [sellToken, buyToken],
+        recipient,
+        Date.now() + 1000 * 60 * 5,
+        {
+          gasPrice: myGasPrice,
+          gasLimit: 210000,
+        }
+      );
+
+      const receipt = await tx.wait();
+      console.log("Sell Transaction receipt");
+      console.log(receipt);
+
+      process.exit();
+    }
+  });
 });
